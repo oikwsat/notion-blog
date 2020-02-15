b816ebae0f13eedb53e2b9a282b29ebb7920448e
oikwsat <oikwsat@gmail.com>
2020-02-15 18:07:48 +0900

    updated index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,12 +10,108 @@ export default () => (
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
       <img
+        src="/rockin-works.png"
+        height="80"
+        width="240"
+        alt="rockin' works"
+      />
+      <h1>My portfolio site.</h1>
+
+      <div className="explanation">
+        <h3>Softwares</h3>
+        <ul>
+          <li>
+            <a
+              href="https://play.google.com/store/apps/details?id=work.rockin.timeprogress"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Time Progress - Google Play のアプリ
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://chrome.google.com/webstore/detail/labelcounter-for-trello/eahijelcdannijiaikdpdpjnhcjoblie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Chromeウェブストア - LabelCounter for Trello
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="explanation">
+        <h3>Magazines</h3>
+        <ul>
+          <li>
+            <a
+              href="http://gihyo.jp/magazine/SD/archive/2013/201304"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Software Design 2013年4月号 ／ 第1特集 僕（私）の言語の学び方
+              裏口からのプログラミング入門 - 今すぐはじめよう！
+              脱超初心者からのベストプラクティス
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://gihyo.jp/book/2012/978-4-7741-5335-3"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Smartphone
+              Design－スマートフォンアプリ開発者とデザイナのための総合情報誌 ／
+              Enterprise Pages
+              業務アプリケーションをスマホやタブレットで使おう！
+              既存のPCサイトをスマートフォン用に変換！
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="explanation">
+        <h3>Links</h3>
+        <ul>
+          <li>
+            <a
+              href="//amzn.to/2e3H0wc"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Amazon
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://medium.com/@oikawa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Medium
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/oikwsat/EffectiveJavaScript/raw/master/build/Effective%20JavaScript%20-%20Dynamic%20Scripting.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download Effective JavaScript - Dynamic Scripting.pdf
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <hr></hr>
+
+      <img
         src="/zeit-and-notion.png"
         height="85"
         width="250"
         alt="ZEIT + Notion"
       />
-      <h1>My Notion Blog</h1>
       <h2>
         Blazing Fast Notion Blog with Next.js'{' '}
         <ExtLink
@@ -26,9 +122,6 @@ export default () => (
           SSG
         </ExtLink>
       </h2>
-
-      <Features />
-
       <div className="explanation">
         <p>
           This is a statically generated{' '}
@@ -43,23 +136,6 @@ export default () => (
           <ExtLink href="https://github.com/zeit/next.js/issues/8626">
             built-in CSS support
           </ExtLink>{' '}
-          which allow us to achieve all of the benefits listed above including
-          blazing fast speeds, great local editing experience, and always being
-          available!
-        </p>
-
-        <p>
-          Get started by creating a new page in Notion and clicking the deploy
-          button below. After you supply your token and the blog index id (the
-          page's id in Notion) we will automatically create the table for you!
-          See{' '}
-          <ExtLink href="https://github.com/oikwsat/notion-blog#getting-blog-index-and-token">
-            here in the readme
-          </ExtLink>{' '}
-          for finding the new page's id. To get your token from Notion, login
-          and look for a cookie under www.notion.so with the name `token_v2`.
-          After finding your token and your blog's page id you should be good to
-          go!
         </p>
       </div>
     </div>

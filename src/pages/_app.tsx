d26a55e1972370fccf5f0493cdc403071346eef3
oikwsat <oikwsat@gmail.com>
2020-02-16 10:38:23 +0900

    feat: add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,22 @@ import ExtLink from '../components/ext-link'
 
 export default ({ Component, pageProps }) => (
   <>
-    <Component {...pageProps} />
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=UA-158637486-1`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'UA-158637486-1');
+        `,
+      }}
+    />
 
+    <Component {...pageProps} />
     <footer></footer>
   </>
 )

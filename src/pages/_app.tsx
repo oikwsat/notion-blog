2bc4d12139b07c749dae5f30ef8715957f92cd5b
oikwsat <oikwsat@gmail.com>
2020-02-18 00:40:03 +0900

    feat: add tweet button

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,21 @@ export default ({ Component, pageProps }) => (
     />
 
     <Component {...pageProps} />
-    <footer></footer>
+
+    <footer>
+      <a
+        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+        className="twitter-share-button"
+        data-via="oikwsat"
+        data-show-count="false"
+      >
+        Tweet
+      </a>
+      <script
+        async
+        src="https://platform.twitter.com/widgets.js"
+        charSet="utf-8"
+      />
+    </footer>
   </>
 )

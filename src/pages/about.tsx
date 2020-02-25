5100cf97b89aa2c4ed5e20901f7f6be79a6bf1ca
oikwsat <oikwsat@gmail.com>
2020-02-25 13:45:38 +0900

    feat: moved content of index to about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -53,6 +53,95 @@ export default () => (
           )
         })}
       </div>
+
+      <hr></hr>
+
+      <div className="explanation">
+        <h3>Softwares</h3>
+        <ul>
+          <li>
+            <a
+              href="https://play.google.com/store/apps/details?id=work.rockin.timeprogress"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Time Progress - Google Play のアプリ
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://chrome.google.com/webstore/detail/labelcounter-for-trello/eahijelcdannijiaikdpdpjnhcjoblie"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Chromeウェブストア - LabelCounter for Trello
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="explanation">
+        <h3>Magazines</h3>
+        <ul>
+          <li>
+            <a
+              href="http://gihyo.jp/magazine/SD/archive/2013/201304"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Software Design 2013年4月号 ／ 第1特集 僕（私）の言語の学び方
+              裏口からのプログラミング入門 - 今すぐはじめよう！
+              脱超初心者からのベストプラクティス
+            </a>
+          </li>
+          <li>
+            <a
+              href="http://gihyo.jp/book/2012/978-4-7741-5335-3"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Smartphone
+              Design－スマートフォンアプリ開発者とデザイナのための総合情報誌 ／
+              Enterprise Pages
+              業務アプリケーションをスマホやタブレットで使おう！
+              既存のPCサイトをスマートフォン用に変換！
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="explanation">
+        <h3>Links</h3>
+        <ul>
+          <li>
+            <a
+              href="//amzn.to/2e3H0wc"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Amazon
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://medium.com/@oikawa"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Medium
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/oikwsat/EffectiveJavaScript/raw/master/build/Effective%20JavaScript%20-%20Dynamic%20Scripting.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download Effective JavaScript - Dynamic Scripting.pdf
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   </>
 )

cdca63106cfad04f3433b202ccae5d49c441e33c
oikwsat <oikwsat@gmail.com>
2020-03-08 16:46:27 +0900

    feat: delete unused footer component

--- src/components/footer.tsx
+++
@@ -1,9 +0,0 @@
-import ExtLink from './ext-link'
-
-export default () => (
-  <>
-    <footer>
-      <ExtLink href="https://github.com/oikwsat/notion-blog">Source code</ExtLink>
-    </footer>
-  </>
-)
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,21 +19,5 @@ export default ({ Component, pageProps }) => (
     />
 
     <Component {...pageProps} />
-
-    <footer>
-      <a
-        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
-        className="twitter-share-button"
-        data-via="oikwsat"
-        data-show-count="false"
-      >
-        Tweet
-      </a>
-      <script
-        async
-        src="https://platform.twitter.com/widgets.js"
-        charSet="utf-8"
-      />
-    </footer>
   </>
 )
